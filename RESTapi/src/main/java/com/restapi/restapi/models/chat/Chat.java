12a1761fa2b18caf44d42eef4ef347cc1c6d4704
Lamers <gustav@henrikssons.org>
2023-05-08 23:53:53 +0200

    amenitie endpoint

---
+++ RESTapi/src/main/java/com/restapi/restapi/models/chat/Chat.java
@@ -0,0 +1,37 @@
+package com.restapi.restapi.models.chat;
+
+import com.restapi.restapi.models.user.User;
+import com.restapi.restapi.models.user.UserMedia;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.List;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Chat {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @ManyToMany
+    @JoinTable
+    private List<Message> messages;
+
+    private Date created;
+    private Date updated;
+    @PrePersist
+    protected void onCreate() {
+        created = new Date();
+    }
+    @PreUpdate
+    protected void onUpdate() {
+        updated = new Date();
+    }
+}
